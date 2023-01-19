2d492592956fa37dfcace9e778d45fb19147e1cf
Mikita Kavaliou <Kent720p@gmail.com>
2023-01-19 21:49:46 +0100

    abstraction of create method

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -2,12 +2,13 @@
 
 class user
 {
-	protected static $db_table = "users";
-	public $id;
-	public $username;
-	public $user_password;
-	public $user_firstname;
-	public $user_lastname;
+	protected static string $db_table = "users";
+	protected array $db_table_fields = ['username', 'user_password', 'user_firstname', 'user_lastname'];
+	public int $id;
+	public string $username;
+	public string $user_password;
+	public string $user_firstname;
+	public string $user_lastname;
 
 	public static function instantiation(array $the_record): user
 	{
@@ -24,6 +25,17 @@ class user
 	{
 		$obj_props = get_object_vars($this);
 		return array_key_exists($the_attribute, $obj_props);
+	}
+
+	protected function get_properties(): array
+	{
+		$props = [];
+		foreach ($this->db_table_fields as $field) {
+			if (property_exists($this, $field)) {
+				$props[$field] = $this->$field;
+			}
+		}
+		return $props;
 	}
 
 	public static function find_this_query(string $sql): array
@@ -47,7 +59,6 @@ class user
 
 	public static function find_user_by_id(int $id): ?user
 	{
-		global $database;
 		$result_arr = self::find_this_query("SELECT * FROM users WHERE id = $id LIMIT 1");
 		return !empty($result_arr) ? array_shift($result_arr) : null;
 
@@ -76,15 +87,9 @@ class user
 	public function create(): bool
 	{
 		global $database;
-		$sql = "
-		INSERT INTO " . self::$db_table . " (username, user_password, user_firstname, user_lastname)
-		VALUES (
-		        '$this->username',
-		        '$this->user_password',
-		        '$this->user_firstname',
-		        '$this->user_lastname'
-		) 
-					";
+		$props = $this->get_properties();
+		$sql = "INSERT INTO " . self::$db_table . "(" . implode(",", array_keys($props)) . ") 
+		VALUES (' " . implode("','", array_values($props)) . " ') ";
 		if ($database->query($sql)) {
 			$this->id = $database->the_insert_id();
 			return true;
