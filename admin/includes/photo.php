3acabbea7cf25c50b2bd11e1db83f81c9dd2b514
Mikita Kavaliou <Kent720p@gmail.com>
2023-01-21 21:24:38 +0100

    wrote fields for photo class

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -3,7 +3,12 @@
 class Photo extends Db_object
 {
 	protected static string $db_table = "photos";
-	protected static array $db_table_fields = ['username', 'user_password', 'user_firstname', 'user_lastname'];
+	protected static array $db_table_fields = ['id', 'title', 'description', 'filename','filesize', 'size'];
 	public ?int $id = null;
+	public ?string $title = null;
+	public ?string $description = null;
+	public ?string $filename = null;
+	public ?string $filetype = null;
+	public ?int $size = null;
 
 }
