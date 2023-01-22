a0c2e3e8a924c464737519f7ab0e58f711bb6a94
Mikita Kavaliou <nik.kow@outlook.com>
2023-01-22 15:12:08 +0100

    modified absolute path constants

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'MAMP' . DS . 'htdocs' . DS . 'loginsys');
+defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'loginsys');
 defined('INCLUDES_PATH') ? null : define('INCLUDES_ROOT', SITE_ROOT . DS . 'admin' . DS . 'includes' );
 defined('ADMIN_ROOT') ? null : define('ADMIN_ROOT', SITE_ROOT . DS . 'admin');
 
