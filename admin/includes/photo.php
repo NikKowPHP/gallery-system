2a5ab469d00d3b0884708700a798e226de650a4a
Mikita Kavaliou <Kent720p@gmail.com>
2023-01-22 13:26:25 +0100

    added fields and set_file method with save function

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -3,12 +3,50 @@
 class Photo extends Db_object
 {
 	protected static string $db_table = "photos";
-	protected static array $db_table_fields = ['id', 'title', 'description', 'filename','filesize', 'size'];
+	protected static array $db_table_fields = ['title', 'description', 'filename','filesize', 'size'];
 	public ?int $id = null;
 	public ?string $title = null;
 	public ?string $description = null;
 	public ?string $filename = null;
 	public ?string $filetype = null;
 	public ?int $size = null;
+
+	public ?string $tmp_path = null;
+	public ?string $upload_dir = "images";
+
+	public array $upload_errors_arr = [];
+	public array $custom_errors = [];
+
+	public function set_file($file)
+	{
+		if(empty($file) || !$file || !is_array($file)) {
+			$this->custom_errors[] = "THERE WAS NO FILE UPLOADED";
+			return false;
+		} elseif ($file['error'] != 0) {
+			$this->custom_errors[] = $this->upload_errors_arr['error'];
+
+		} else {
+			$this->filename = basename($file['name']);
+			$this->tmp_path = $file['tmp_name'];
+			$this->filetype = $file['type'];
+			$this->size = $file['size'];
+
+		}
+	}
+	public function save()
+	{
+		if ($this->id) {
+			$this->update();
+		} else {
+			if(!empty($this->custom_errors)) {
+				return false;
+			}
+			if (empty($this->filename) || empty($this->tmp_path)) {
+				$this->custom_errors[] = "the file was not available";
+				return false;
+			}
+			$target_path = ADMIN_ROOT . $this->upload_dir . DS . $this->filename;
+		}
+	}
 
 }
