ddaac9e744539cc47ac093726b68f67a343b740a
Mikita Kavaliou <Kent720p@gmail.com>
2023-01-22 12:19:35 +0100

    created constants of paths

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,9 +1,16 @@
 <?php
+
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'MAMP' . DS . 'htdocs' . DS . 'loginsys' . DS);
+defined('INCLUDES_PATH') ? null : define('INCLUDES_ROOT', SITE_ROOT . DS . 'admin' . DS . 'includes' . DS);
+
 require_once("new_config.php");
 require_once("database.php");
 require_once("db_object.php");
 require_once("User.php");
+require_once("photo.php");
 require_once("functions.php");
 require_once("session.php");
+
 
 
