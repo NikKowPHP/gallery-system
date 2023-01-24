ead829dc8c40baa2073fdf8aa4a4dc44df0f5396
Mikita Kavaliou <nik.kow@outlook.com>
2023-01-24 21:56:12 +0100

    fixed bug with update method, created iterate through POST superglobal

--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -2,7 +2,7 @@
 
 class Db_object
 {
-	private static function instantiate($db_row):?self
+	private static function instantiate($db_row): ?self
 	{
 		$calling_class = get_called_class();
 		$obj = new $calling_class;
@@ -27,12 +27,12 @@ class Db_object
 		return $props;
 	}
 
-	protected static function get_data_by_query($sql):array
+	protected static function get_data_by_query($sql): array
 	{
 		global $database;
 		$objects_arr = [];
 		$rows = $database->query($sql);
-		while($row = $rows->fetch_assoc() ) {
+		while ($row = $rows->fetch_assoc()) {
 			$objects_arr[] = static::instantiate($row);
 		}
 		return $objects_arr;
@@ -84,8 +84,7 @@ class Db_object
 		foreach ($props as $key => $value) {
 			$props_pairs[] = "$key='$value'";
 		}
-		$sql = "
-		UPDATE users SET " . implode(", ", $props_pairs) ." WHERE id = $this->id";
+		$sql = "UPDATE " . static::$db_table . " SET " . implode(", ", $props_pairs) . " WHERE id = $this->id";
 		$database->query($sql);
 		return $database->connection->affected_rows === 1;
 
@@ -97,6 +96,20 @@ class Db_object
 		$sql = "
 		DELETE FROM " . static::$db_table . " WHERE id = $this->id LIMIT 1";
 		return (bool)$database->query($sql);
+	}
+
+	public function iterate_post($post):static
+	{
+
+		foreach ($post as $key => $value) {
+			if(property_exists($this, $key)) {
+				$this->$key = $value;
+			}
+		}
+		echo "<pre>";
+		print_r($this);
+		echo "</pre>";
+		return $this;
 	}
 
 }
