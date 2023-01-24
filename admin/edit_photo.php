ef2279d8300cf7f49cb989b71a96614eec596a2c
Mikita Kavaliou <nik.kow@outlook.com>
2023-01-24 18:35:45 +0100

    get all info from Photo object in edit photo.php

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -1,6 +1,12 @@
 <?php include("includes/header.php"); ?>
 <?php include("includes/top_nav.php"); ?>
 
+<?php
+if (isset($_GET['id'])) $photo = Photo::get_by_id($_GET['id']);
+?>
+
+    }
+    ?>
 
 
     <div class="container-fluid">
@@ -17,30 +23,40 @@
                         <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
                     </li>
                     <li class="active">
-                        <i class="fa fa-file"></i> Blank Page
+                        <i class="fa fa-file"></i> Edit Photo <?= $photo->id ?>
                     </li>
                 </ol>
             </div>
         </div>
         <!-- /.row -->
-        <div class="col-md-12">
-            <div class="form-group">
-                <label> caption
-                    <input type="text" name="title" class="form-control">
-                </label>
+        <form action="edit_photo.php" method="post" enctype="multipart/form-data">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label> Title
+                        <input type="text" name="title" class="form-control" value="<?= $photo->title ?>">
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label> Alternate text
+                        <input type="text" name="alt" class="form-control" value="<?= $photo->alt ?>">
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label>Description
+                        <textarea name="description" id="" cols="30" rows="10"
+                                  class="form-control"><?= $photo->description ?></textarea>
+                    </label>
+                </div>
+                <input class="btn btn-primary" type="submit" name="submit" value="submit">
             </div>
-            <div class="form-group">
-                <label> Alternate text
-                    <input type="text" name="description" class="form-control">
-                </label>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <img width="500" src="<?= $photo->get_file_path() ?>" alt="">
+
+
+                </div>
             </div>
-            <div class="form-group">
-                <label>Description
-                    <textarea name="description" id="" cols="30" rows="10" class="form-control"></textarea>
-                </label>
-            </div>
-            <input type="submit" name="submit" value="submit">
-        </div>
+        </form>
         <!-- /.container-fluid -->
 			<?php include_once("includes/sidebar_nav.php"); ?>
 
