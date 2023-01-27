fc203823d13ab0a3cf98b2a0711b15a89abdb3df
Mikita Kavaliou <nik.kow@outlook.com>
2023-01-27 18:10:09 +0100

    created the logic of add user

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -2,23 +2,29 @@
 <?php if (!$session->is_signed_in()) redirect("login_page.php"); ?>
 
 <?php
-if (empty($_GET['id'])) redirect("admin/users.php");
 
 if (isset($_POST['submit'])) {
-	if ($user = User::get_by_id($_GET['id'])) {
-		$edited_user = $user->iterate_post($_POST);
-// TODO: AVATAR UPLOAD AND DELETE
-//		if ($_FILES['new_photo_upload']['size'] !== 0) {
-//			if (file_exists($user->unlink_path())) {
-//				unlink($user->unlink_path());
-//			}
-//			$edited_user->set_file($_FILES['new_photo_upload']);
-//			$edited_user->update_file();
-//		}
-		$edited_user->save();
-		redirect("admin/edit_user_page.php/?id=$edited_user->id");
-	}
+	$user = new User();
+	echo "<pre>";
+	print_r($_FILES['avatar_file']);
+	echo "</pre>";
 
+	if ($_FILES['avatar_file']['error'] === 0) {
+		$user->avatar = $_FILES['avatar_file']['name'];
+		$file = new File($user->upload_dir);
+		$file->init($_FILES['avatar_file']);
+		$file->upload();
+	}
+	$user = $user->iterate_post($_POST);
+	echo "<pre>";
+	print_r($user);
+	echo "</pre>";
+
+	if($user->save()) {
+		$session->message = "The user $user->username successfully created";
+		redirect("admin/users.php");
+
+	}
 
 }
 ?>
