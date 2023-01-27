df37caa79115d83b52a3b00a71d2b54f471353ad
Mikita Kavaliou <nik.kow@outlook.com>
2023-01-27 18:06:26 +0100

    created class File, and created methods

---
+++ admin/add_user_page.php
@@ -0,0 +1,74 @@
+<?php include("includes/header.php"); ?>
+<?php include("includes/top_nav.php"); ?>
+
+<?php
+if (isset($_GET['id'])) {
+	$user = User::get_by_id($_GET['id']);
+} else {
+	redirect("admin/users.php");
+}
+?>
+    <div id="page-wrapper">
+
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                   Users
+                    <small>Create User</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li>
+                        <i class="fa fa-dashboard"></i> <a href="/loginsys/admin/">Dashboard</a>
+                    </li>
+                    <li class="active">
+                        <i class="fa fa-file"></i> Create user
+                    </li>
+                </ol>
+            </div>
+        </div>
+
+
+        <!-- /.row -->
+        <form action="add_user.php" method="POST" enctype="multipart/form-data">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label> username
+                        <input type="text" name="username" class="form-control" value="<?= $user->username ?>">
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label> password
+                        <input type="text" name="password" class="form-control" value="<?= $user->username ?>">
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label> firstname
+                        <input type="text" name="firstname" class="form-control" value="<?= $user->firstname ?>">
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label>lastname
+                        <input type="text" name="lastname" class="form-control" value="<?= $user->lastname?>">
+                    </label>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <img width="500" src="<?= $user->avatar_placeholder_path() ?>" alt="user avatar">
+                </div>
+                <div class="form-group">
+                    <input type="file" name="new_photo_upload" class="form-control">
+                </div>
+                <input class="btn btn-primary" type="submit" name="submit" value="Edit">
+            </div>
+        </form>
+        <!-- /.container-fluid -->
+			<?php include_once("includes/sidebar_nav.php"); ?>
+
+    </div>
+    <!-- /#page-wrapper -->
+
+<?php include("includes/footer.php"); ?>
---
+++ admin/includes/File.php
@@ -0,0 +1,87 @@
+<?php
+
+class File extends Db_object
+{
+//	protected static string $db_table = "photos";
+//	protected static array $db_table_fields = ['title','alt', 'description', 'filename', 'filetype', 'size'];
+//	public ?int $id = null;
+//	public ?string $title = null;
+//	public ?string $alt = null;
+//	public ?string $description = null;
+
+	public ?string $name = null;
+	public ?string $type = null;
+	public ?int $size = null;
+
+	public ?string $tmp_path = null;
+	public ?string $upload_dir = null;
+
+	public array $errors = array(
+		0 => 'There is no error, the file uploaded with success',
+		1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+		2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+		3 => 'The uploaded file was only partially uploaded',
+		4 => 'No file was uploaded',
+		6 => 'Missing a temporary folder',
+		7 => 'Failed to write file to disk.',
+		8 => 'A PHP extension stopped the file upload.',
+
+	);
+	public array $custom_errors = [];
+
+
+	public function __construct(string $upload_dir)
+	{
+		$this->upload_dir = $upload_dir;
+	}
+
+	public function path(): string
+	{
+		return ADMIN_UPLOADS_PATH . DS . $this->upload_dir . DS . $this->name;
+	}
+
+
+	public function init(array $file): bool
+	{
+		if (empty($file)) {
+			$this->custom_errors[] = "THERE WAS NO FILE UPLOADED";
+			return false;
+		} elseif ($file['error'] != 0) {
+			$this->custom_errors[] = $this->errors[$file['error']];
+			return false;
+		} else {
+			$this->name = basename($file['name']);
+			$this->tmp_path = $file['tmp_name'];
+			$this->type = $file['type'];
+			$this->size = $file['size'];
+			return true;
+		}
+	}
+
+	public function upload(): bool
+	{
+		if (empty($this->name) || empty($this->tmp_path)) {
+			$this->custom_errors[] = "the file was not available";
+			return false;
+		}
+		$target_path = ADMIN_ROOT . DS . $this->upload_dir . DS . $this->name;
+
+		if (file_exists($target_path)) {
+			$this->custom_errors[] = "the file $this->name already exists";
+			return false;
+		}
+		if (move_uploaded_file($this->tmp_path, $target_path)) {
+			unset($this->tmp_path);
+			return true;
+		} else {
+			$this->custom_errors[] = "the file directory does not have permission";
+			return false;
+		}
+	}
+
+	public function delete(): bool
+	{
+		return unlink(ADMIN_ROOT . DS . $this->upload_dir . DS . $this->name) ?? true;
+	}
+
+}
