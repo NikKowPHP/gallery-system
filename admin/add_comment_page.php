9993031e0916affd14b02af0d8cb66b49ac7c3dd
Mikita Kavaliou <nik.kow@outlook.com>
2023-01-28 11:45:55 +0100

    created new file add_comment_page

---
+++ admin/add_comment_page.php
@@ -0,0 +1,64 @@
+<?php include("includes/header.php"); ?>
+<?php include("includes/top_nav.php"); ?>
+
+    <div id="page-wrapper">
+
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                   comments
+                    <small>Create comment</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li>
+                        <i class="fa fa-dashboard"></i> <a href="/loginsys/admin/">Dashboard</a>
+                    </li>
+                    <li class="active">
+                        <i class="fa fa-file"></i> Create comment
+                    </li>
+                </ol>
+            </div>
+        </div>
+
+
+        <!-- /.row -->
+        <form action="/loginsys/admin/includes/add_comment.php" method="POST" enctype="multipart/form-data">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label> commentname
+                        <input type="text" name="commentname" class="form-control" >
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label> password
+                        <input type="password" name="password" class="form-control" >
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label> firstname
+                        <input type="text" name="firstname" class="form-control">
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label>lastname
+                        <input type="text" name="lastname" class="form-control" >
+                    </label>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <input type="file" name="avatar_file" class="form-control">
+                </div>
+                <input class="btn btn-primary" type="submit" name="submit" value="Create">
+            </div>
+        </form>
+        <!-- /.container-fluid -->
+			<?php include_once("includes/sidebar_nav.php"); ?>
+
+    </div>
+    <!-- /#page-wrapper -->
+
+<?php include("includes/footer.php"); ?>
