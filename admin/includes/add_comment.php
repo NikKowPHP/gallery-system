8d629fe0d967bb83c92ec33204e6c27828eeb9a2
Mikita Kavaliou <nik.kow@outlook.com>
2023-01-28 11:39:58 +0100

    created add_comment page

---
+++ admin/includes/add_comment.php
@@ -0,0 +1,18 @@
+<?php include("init.php"); ?>
+<?php if (!$session->is_signed_in()) redirect("login_page.php"); ?>
+
+<?php
+
+if (isset($_POST['submit'])) {
+	$comment = new Comment();
+
+	$comment = $comment->iterate_post($_POST);
+
+	if($comment->save()) {
+		$session->message = "The comment to photo id '$comment->photo_id' successfully created";
+		redirect("admin/comments.php");
+
+	}
+
+}
+?>
