5f915c24fddf0e5e5c441b6021eaf89584ef13d7
Mikita Kavaliou <nik.kow@outlook.com>
2023-01-28 11:32:12 +0100

    added fileds for comment class

--- admin/includes/Comment.php
+++ admin/includes/Comment.php
@@ -2,4 +2,14 @@
 class Comment extends Db_object
 {
 
+	protected static $db_table = "comments";
+	protected static $db_table_fields = ['id', 'photo_id', 'user_id', 'body', 'date'];
+	public ?int $id = null;
+	public ?int $photo_id = null;
+	public ?int $user_id = null;
+	public ?string $body = null;
+	public ?string $date = null;
+
+
+
 }
