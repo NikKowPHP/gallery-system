af3259be1bb0c78834282e1088b4223fe2720a5b
Mikita Kavaliou <nik.kow@outlook.com>
2023-01-29 18:17:29 +0100

    show all comments by photo

--- admin/comments.php
+++ admin/comments.php
@@ -2,75 +2,80 @@
 <?php include("includes/top_nav.php"); ?>
 
 
-<div id="page-wrapper">
+    <div id="page-wrapper">
 
 
-    <div class="container-fluid">
+        <div class="container-fluid">
 
-        <!-- Page Heading -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">
-                    Comments
-                    <small>Subheading</small>
-                </h1>
-                <ol class="breadcrumb">
-                    <li>
-                        <i class="fa fa-dashboard"></i> <a href="index.php">Dashboard</a>
-                    </li>
-                    <li class="active">
-                        <i class="fa fa-file"></i> Comments
-                    </li>
-                </ol>
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Comments
+                        <small>Subheading</small>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li>
+                            <i class="fa fa-dashboard"></i> <a href="index.php">Dashboard</a>
+                        </li>
+                        <li class="active">
+                            <i class="fa fa-file"></i> Comments
+                        </li>
+                    </ol>
 
-							<?php if(isset($session->message)) {
-								echo "<h4>$session->message</h4>";
-							} ?>
+									<?php if (isset($session->message)) {
+										echo "<h4>$session->message</h4>";
+									} ?>
 
-                <a href="/loginsys/admin/add_comment_page.php" class="btn btn-info"> create comment</a>
+                    <a href="/loginsys/admin/add_comment_page.php" class="btn btn-info"> create comment</a>
+                </div>
+            </div>
+            <!-- /.row -->
+            <div class="col-md-12">
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>photo_id</th>
+                        <th>user_id</th>
+                        <th>body</th>
+                        <th>date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+											<?php
+											if (!isset($_GET['id'])) {
+												$comments = Comment::get_all();
+											} else {
+                                                $comments = Comment::get_all_by("photo_id", $_GET['id']);
+                                            }
+
+											foreach ($comments
+
+											as $comment):
+											?>
+                        <td><?= $comment->id ?></td>
+                        <td><?= $comment->photo_id ?></td>
+                        <td><?= $comment->user_id ?></td>
+                        <td><?= $comment->body ?></td>
+                        <td><?= $comment->date ?></td>
+                        <td><a class="btn btn-primary" href="edit_comment_page.php/?id=<?= $comment->id ?>">edit</a>
+                        </td>
+                        <td><a class="btn btn-danger" href="delete_comment.php/?id=<?= $comment->id ?>">delete</a></td>
+                    </tr>
+										<?php endforeach; ?>
+                    </tbody>
+                </table>
+
             </div>
         </div>
         <!-- /.row -->
-        <div class="col-md-12">
-            <table class="table table-hover">
-                <thead>
-                <tr>
-                    <th>id</th>
-                    <th>photo_id</th>
-                    <th>user_id</th>
-                    <th>body</th>
-                    <th>date</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-									<?php
-									$comments = Comment::get_all();
-                                    
-									foreach ($comments
 
-									as $comment):
-									?>
-                    <td><?= $comment->id ?></td>
-                    <td><?= $comment->photo_id?></td>
-                    <td><?= $comment->user_id?></td>
-                    <td><?= $comment->body?></td>
-                    <td><?= $comment->date?></td>
-                    <td><a class="btn btn-primary" href="edit_comment_page.php/?id=<?= $comment->id ?>">edit</a></td>
-                    <td><a class="btn btn-danger" href="delete_comment.php/?id=<?= $comment->id ?>">delete</a></td>
-                </tr>
-								<?php endforeach; ?>
-                </tbody>
-            </table>
+        <!-- /.container-fluid -->
+			<?php include_once("includes/sidebar_nav.php"); ?>
 
-            </div>
-        </div>
-        <!-- /.row -->
-
-    <!-- /.container-fluid -->
-    <?php include_once("includes/sidebar_nav.php"); ?>
-
-</div>
-<!-- /#page-wrapper -->
+    </div>
+    <!-- /#page-wrapper -->
 
 <?php include("includes/footer.php"); ?>
