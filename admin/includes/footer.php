826999e175ff0837b1cf189d81b95eabe9a9d444
Mikita Kavaliou <nik.kow@outlook.com>
2023-02-01 17:57:30 +0100

    added jquery links and created a modal

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,10 +1,17 @@
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+<!--    <script src="js/jquery.js"></script>-->
+
+    <script
+            src="https://code.jquery.com/jquery-3.6.3.min.js"
+            integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
+            crossorigin="anonymous"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
   <script src="js/script.js"></script>
+
     <script>
 
         google.charts.load('current', {'packages':['corechart']});
@@ -31,6 +38,26 @@
 
             chart.draw(data, options);
         }
+
+
+
+    </script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            let image_id;
+            let user_id = $("#user_id").data('user-id');
+            $("#user_image").click(function () {
+                alert(user_id);
+
+                $('#modal1').modal();
+                $(".modal_thumbnail").click(function () {
+                    image_id = $(this).data('id');
+                    console.log(image_id)
+                    $("#set_user_image").prop("disabled", false);
+                })
+
+            });
+        });
     </script>
 
 </body>
