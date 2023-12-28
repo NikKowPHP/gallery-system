2dde12e0d0414464a0c9e04c1060d546913a3553
NikKowPHP <nik.kow@outlook.com>
2023-12-28 07:26:33 +0100

    feat(registration_page): Add registration page component
    Create the Registration Page component to facilitate user registration within the application. This addition establishes the foundation for user registration functionality.

---
+++ registration_page.php
@@ -0,0 +1,33 @@
+<?php require_once("./admin/includes/login.php") ?>
+<?php require_once("./includes/header.php") ?>
+
+<div class="col-md-4 col-md-offset-3">
+	<h3>Registration</h3>
+
+
+<form id="login-id" action="./admin/includes/add_user.php" method="post">
+
+	<div class="form-group">
+		<label for="username">Username</label>
+		<input type="text" class="form-control" name="username" value="<?php echo htmlentities($username); ?>" >
+
+	</div>
+
+	<div class="form-group">
+		<label for="password">Password</label>
+		<input type="password" class="form-control" name="password" value="<?php echo htmlentities($password); ?>">
+
+	</div>
+
+
+	<div class="form-group">
+		<input type="submit" name="submit" value="Submit" class="btn btn-primary">
+
+	</div>
+
+
+</form>
+
+    <h4 class="bg-danger"><?= $session->message ?? "" ?></h4>
+
+</div>
