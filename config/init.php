e7934f9f3feda3773e0151f3c3bc4a213b461793
NikKowPHP <nik.kow@outlook.com>
2023-12-29 12:34:24 +0100

    feat(init): Introduce PUBLIC_FOLDER constant and autoload File class
    Add the PUBLIC_FOLDER constant for better file management and autoload the File class for improved code organization. This addition enhances file-related operations and simplifies class loading within the project.

--- config/init.php
+++ config/init.php
@@ -3,10 +3,11 @@
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'code' . DS . 'gallery-system');
 defined('SRC') ? null : define('SRC', SITE_ROOT . DS . 'src');
+defined('PUBLIC_FOLDER') ? null : define('PUBLIC_FOLDER', SITE_ROOT . DS . 'public');
 
 
 
-$required_files = ["/src/core/database.php","/src/models/Db_object.php", "/src/models/Photo.php"];
+$required_files = ["/src/core/database.php","/src/models/Db_object.php", "/src/models/Photo.php", "/src/models/File.php"];
 
 foreach ($required_files as $file) {
     require_once(SITE_ROOT . $file);
