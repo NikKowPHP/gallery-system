45c09c8396dba6604c136154c78f9487e0e2c302
NikKowPHP <nik.kow@outlook.com>
2023-12-29 12:32:28 +0100

    refactor(Photo): Enhance Photo class by utilizing the File class, remove redundant code
    Improve the Photo class functionality by integrating the File class, eliminating duplicated code segments. This refactor streamlines the codebase by leveraging File class features and reducing redundancy within the Photo class.

--- src/models/Photo.php
+++ src/models/Photo.php
@@ -3,100 +3,57 @@
 class Photo extends Db_object
 {
 	protected static string $db_table = "photos";
-	protected static array $db_table_fields = ['title','alt', 'description', 'filename', 'filetype', 'size'];
+	protected static array $db_table_fields = ['title', 'alt', 'description', 'file_id' ];
 	public ?int $id = null;
 	public ?string $title = null;
 	public ?string $alt = null;
 	public ?string $description = null;
-	public ?string $filename = null;
-	public ?string $filetype = null;
-	public ?int $size = null;
-
-	public ?string $tmp_path = null;
+	public ?File $file = null;
 	public ?string $upload_dir = "images";
 
-	public array $upload_errors_arr = [];
-	public array $custom_errors = [];
-
-	public function unlink_path():string
+	public function __construct(File $file, string $title, string $alt)
 	{
-		return ADMIN_ROOT . DS . $this->upload_dir . DS . $this->filename;
+		$this->title = $title;
+		$this->alt = $alt;
+		$this->setFile($file);
+	}
+	public function setDescription(string $description)
+	{
+		$this->description = $description;
 	}
 
-	public function get_file_path():string
+	public function setFile(File $file): void
 	{
-		return ADMIN_UPLOADS_PATH . DS . $this->upload_dir . DS . $this->filename;
+		$this->file = $file;
 	}
 
-
-	public function set_file($file)
-	{
-		if (empty($file) || !$file || !is_array($file)) {
-			$this->custom_errors[] = "THERE WAS NO FILE UPLOADED";
-			return false;
-		} elseif ($file['error'] != 0) {
-			$this->custom_errors[] = $this->upload_errors_arr['error'];
-
-		} else {
-			$this->filename = basename($file['name']);
-			$this->tmp_path = $file['tmp_name'];
-			$this->filetype = $file['type'];
-			$this->size = $file['size'];
-
-		}
-	}
-
-	public function save()
+	public function save(): bool
 	{
 		if ($this->id) {
 			$this->update();
-		} else {
-			if (!empty($this->custom_errors)) {
-				return false;
-			}
-			if (empty($this->filename) || empty($this->tmp_path)) {
-				$this->custom_errors[] = "the file was not available";
-				return false;
-			}
-			$target_path = ADMIN_ROOT . DS . $this->upload_dir . DS . $this->filename;
-
-			if (file_exists($target_path)) {
-				$this->custom_errors[] = "the file $this->filename already exists";
-			}
-			if (move_uploaded_file($this->tmp_path, $target_path)) {
-				if ($this->create()) {
-					unset($this->tmp_path);
-					return true;
-				}
-			} else {
-				$this->custom_errors[] = "the file directory does not have permission";
-			}
-
+			return true;
 		}
-	}
-
-	public function update_file():bool
-	{
-		$target_path = ADMIN_ROOT . DS . $this->upload_dir . DS . $this->filename;
-		if (file_exists($target_path)) {
-			$this->custom_errors[] = "the file $this->filename already exists";
-		}
-		if (move_uploaded_file($this->tmp_path, $target_path)) {
-				unset($this->tmp_path);
-				return true;
-		} else {
-			$this->custom_errors[] = "the file directory does not have permission";
-		}
-
-	}
-
-	public function delete_photo(): bool
-	{
-		if ($this->delete()) {
-			return unlink($this->unlink_path()) ?? true;
-		} else {
+		if(!$this->file->upload()) {
 			return false;
 		}
+
+		if($this->create()) {
+			return true;
+		} else {
+			$this->file->remove($this->upload_dir, $this->file->name);
+			return false;
+		}
+
+	}
+
+	public function update(): bool
+	{
+
+	}
+
+	public function remove(): bool
+	{
+
 	}
 
 
