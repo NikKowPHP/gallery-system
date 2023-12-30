c4b031ce624cffd54a45210e723a9202a984a395
NikKowPHP <nik.kow@outlook.com>
2023-12-30 19:58:39 +0100

    feat(autoload): Update autoload to load classes based on the provided paths

--- autoload.php
+++ autoload.php
@@ -2,31 +2,33 @@
 
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'code' . DS . 'gallery-system');
+defined('FORMS_PATH') ? null : define('FORMS_PATH', DS . 'gallery-system');
 defined('SRC') ? null : define('SRC', SITE_ROOT . DS . 'src');
 defined('PUBLIC_FOLDER') ? null : define('PUBLIC_FOLDER', SITE_ROOT . DS . 'public');
 
 spl_autoload_register(function ($class_name) {
-	// print_r(substr($class_name, 0, 10));
+	$paths = [
+		'Models' => SITE_ROOT . '/src/models/',
+		'Utils' => SITE_ROOT . '/src/utils/',
+		'Templates' => SITE_ROOT . '/src/views/templates/',
+		'Core' => SITE_ROOT . '/src/core/',
+	];
+	foreach ($paths as $namespace => $path) {
+		if (substr($class_name, 0, strlen($namespace)) === $namespace) {
+			$file = $path . substr($class_name, strlen($namespace) + 1) . '.php';
 
-    if (substr($class_name, 0, 7) === 'Models\\') {
-        // Load models from 'src/models' folder
-
-        $file = SITE_ROOT . '/src/models/' . substr($class_name, 7) . '.php';
-
-    } elseif (substr($class_name, 0, 13) === 'Utils\\Functions\\') {
-        $file = SITE_ROOT . '/src/utils/' . substr($class_name, 12) . '.php';
-    } elseif (substr($class_name, 0, 10) === 'Templates\\'){
-
-        $file = SITE_ROOT . '/src/views/templates/' . substr($class_name, 10) . '.php';
-				var_dump($file);
-
+			if (file_exists($file)) {
+				require_once($file);
+				return true;
+			}
 		}
-		 else {
-        // Load other classes from 'src' folder
-        $file = SITE_ROOT . '/src/' . str_replace('\\', DS, $class_name) . '.php';
-    }
+	}
 
-    if (file_exists($file)) {
-        require_once($file);
-    }
+	// If the class was not found in any of the specified paths, return false
+	var_dump($file);
+	return false;
+
+	if (file_exists($file)) {
+		require_once($file);
+	}
 });
