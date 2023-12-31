6108d6d72ed3fb2cbe17292d1adb040a4e95f2c0
NikKowPHP <nik.kow@outlook.com>
2023-12-31 10:42:18 +0100

    feat(login_page_content): Introduce an encapsulated component that renders only login form.
    This implementation provides with better code organization and code readability

---
+++ src/views/contents/login_page_content.php
@@ -0,0 +1,29 @@
+<div class="col-md-4 col-md-offset-3">
+	<form id="login-id" action=<?=FORMS_PATH . DS."admin/includes/login.php" ?> method="post">
+
+		<div class="form-group">
+			<label for="username">Username</label>
+			<input type="text" class="form-control" name="username" value="">
+
+		</div>
+
+		<div class="form-group">
+			<label for="password">Password</label>
+			<input type="password" class="form-control" name="password" value="">
+
+		</div>
+
+
+		<div class="form-group">
+			<input type="submit" name="submit" value="Submit" class="btn btn-primary">
+
+		</div>
+
+
+	</form>
+
+	<h4 class="bg-danger">
+		<?= $session->message ?? "" ?>
+	</h4>
+
+</div>
