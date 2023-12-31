f1f54d4c830412809901f5ba3dffccd37addfa1f
NikKowPHP <nik.kow@outlook.com>
2023-12-31 10:49:11 +0100

    refactor(Db_object): Refine database object class with static database instance, in static methods check for the instance existence

--- src/models/Db_object.php
+++ src/models/Db_object.php
@@ -1,7 +1,28 @@
 <?php
 namespace Models;
+
+use Core\Database;
+
 class Db_object
 {
+	protected static ?Database $database = null;
+
+	public function __construct()
+	{
+		self::$database = Database::get_instance();
+	}
+
+	private static function check_database_instance(): void
+	{
+		if (!isset(self::$database)) {
+			self::set_database(Database::get_instance());
+		}
+	}
+
+	public static function set_database(Database $database)
+	{
+		self::$database = $database;
+	}
 	private static function instantiate($db_row): ?self
 	{
 		$calling_class = get_called_class();
@@ -17,47 +38,43 @@ class Db_object
 
 	protected function properties(): array
 	{
-		global $database;
 		$props = [];
 		foreach (static::$db_table_fields as $field) {
 			if (property_exists($this, $field)) {
-				$props[$field] = $database->escape_string($this->$field);
+				$props[$field] = self::$database->escape_string($this->$field);
 			}
 		}
 		return $props;
 	}
 
-	public static function get_data_by_query($sql): array
+	public static function get_data_by_query($sql): array|bool
 	{
-		global $database;
+		self::check_database_instance();
+
 		$objects_arr = [];
-		$rows = $database->query($sql);
+		$rows = self::$database->query($sql);
+		var_dump($rows);
 		while ($row = $rows->fetch_assoc()) {
 			$objects_arr[] = static::instantiate($row);
 		}
 		return $objects_arr;
-
 	}
 
 	public static function get_all(): array|bool
 	{
-		$query = "SELECT * FROM " . static::$db_table;
-		$result_set = static::get_data_by_query($query);
-		if(!$result_set || empty($result_set)) {
+		$sql = "SELECT * FROM " . static::$db_table;
+		$result_set = static::get_data_by_query($sql);
+		if (!$result_set || empty($result_set)) {
 			return false;
 		}
 		return $result_set;
-
-
 	}
 
-	public static function get_by_id(int $id): ?self
+	public static function find(int $id): ?self
 	{
 		$sql = "SELECT * FROM " . static::$db_table . " WHERE id = $id";
 		$result_arr = self::get_data_by_query($sql);
 		return !empty($result_arr) ? array_shift($result_arr) : null;
-
-
 	}
 
 	public function save()
@@ -68,12 +85,17 @@ class Db_object
 
 	public function create(): bool
 	{
-		global $database;
 		$props = $this->properties();
-		$sql = "INSERT INTO " . static::$db_table . "(" . implode(",", array_keys($props)) . ") 
-		VALUES (' " . implode("','", array_values($props)) . " ') ";
-		if ($database->query($sql)) {
-			$this->id = $database->the_insert_id();
+		$columns = implode(", ", array_keys($props));
+		$values = "'" . implode("','", array_values($props)) . "'";
+		// TODO: Prepare statements 
+
+
+
+		$sql = "INSERT INTO " . static::$db_table . " ($columns) VALUES ($values)";
+		var_dump($sql);
+		if (self::$database->query($sql)) {
+			$this->id = self::$database->insert_id();
 			return true;
 		} else {
 			return false;
@@ -82,7 +104,6 @@ class Db_object
 
 	public function update(): bool
 	{
-		global $database;
 
 		$props = $this->properties();
 		$props_pairs = [];
@@ -91,24 +112,23 @@ class Db_object
 			$props_pairs[] = "$key='$value'";
 		}
 		$sql = "UPDATE " . static::$db_table . " SET " . implode(", ", $props_pairs) . " WHERE id = $this->id";
-		$database->query($sql);
-		return $database->connection->affected_rows === 1;
+		self::$database->query($sql);
+		return self::$database->connection->affected_rows === 1;
 
 	}
 
 	public function delete(): bool
 	{
-		global $database;
 		$sql = "
 		DELETE FROM " . static::$db_table . " WHERE id = $this->id LIMIT 1";
-		return (bool)$database->query($sql);
+		return (bool) self::$database->query($sql);
 	}
 
-	public function iterate_post($post):static
+	public function iterate_post($post): static
 	{
 
 		foreach ($post as $key => $value) {
-			if(property_exists($this, $key)) {
+			if (property_exists($this, $key)) {
 				$this->$key = trim($value);
 			}
 		}
@@ -116,17 +136,16 @@ class Db_object
 	}
 	public static function count(?string $by = null, ?int $id = null): int
 	{
-		global $database;
-		if(is_null($by) && is_null($id)) {
+		self::check_database_instance();
+		if (is_null($by) && is_null($id)) {
 			$sql = "SELECT COUNT('id') FROM " . static::$db_table;
 		} else {
-			$sql = "SELECT COUNT('id') FROM " . static::$db_table ." WHERE $by = $id";
+			$sql = "SELECT COUNT('id') FROM " . static::$db_table . " WHERE $by = $id";
+			var_dump($sql);
 		}
-		$data = $database->query($sql);
+		
+		$data = self::$database->query($sql);
 		$fetch = $data->fetch_array();
-		return (int)array_shift($fetch);
-
-
+		return (int) array_shift($fetch);
 	}
-
 }
