04a01e2be404cae3368dbf7a848ebcd5788dae1f
NikKowPHP <nik.kow@outlook.com>
2023-12-31 10:31:55 +0100

    feat(login_page): Refine component with using template based layout to prevent mixing html with php

--- src/views/login_page.php
+++ src/views/login_page.php
@@ -1,32 +1,13 @@
-<?php require_once("./admin/includes/login.php") ?>
-<?php require_once("./includes/header.php") ?>
+<?php require_once(__DIR__ . "/../../autoload.php"); ?>
 
-<div class="col-md-4 col-md-offset-3">
+<?php require_once(SITE_ROOT . DS . "admin/includes/login.php") ?>
+<?php
+use Templates\HomeTemplate;
 
-
-<form id="login-id" action="./admin/includes/login.php" method="post">
-
-	<div class="form-group">
-		<label for="username">Username</label>
-		<input type="text" class="form-control" name="username" value="<?php echo htmlentities($username); ?>" >
-
-	</div>
-
-	<div class="form-group">
-		<label for="password">Password</label>
-		<input type="password" class="form-control" name="password" value="<?php echo htmlentities($password); ?>">
-
-	</div>
+$body = "/src/views/contents/login_page_content.php";
+$template = new HomeTemplate();
+$template->setBody($body);
 
 
-	<div class="form-group">
-		<input type="submit" name="submit" value="Submit" class="btn btn-primary">
-
-	</div>
-
-
-</form>
-
-    <h4 class="bg-danger"><?= $session->message ?? "" ?></h4>
-
-</div>
+$template->render();
+?>
