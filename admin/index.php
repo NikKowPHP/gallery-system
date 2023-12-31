abcd0dce6681767ee5b43003230aa16745581721
NikKowPHP <nik.kow@outlook.com>
2023-12-31 10:26:06 +0100

    refactor(index.admin): Restructurize component to use a template based layout

--- admin/index.php
+++ admin/index.php
@@ -1,131 +1,24 @@
-<?php include("includes/header.php"); ?>
-<?php include("includes/top_nav.php"); ?>
+<?php
+use Models\Session;
+use Models\Location;
+
+require_once(__DIR__ . '/../src/views/templates/AdminTemplate.php');
+
+$session = new Session();
+
+if (!$session->is_signed_in()) Location::redirect("src/views/login_page.php");
 
 
-    <div id="page-wrapper">
-
-        <div class="container-fluid">
-
-            <!-- Page Heading -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">
-                        Admin Dashboard
-                        <small>Subheading</small>
-                    </h1>
-                    <ol class="breadcrumb">
-                        <li>
-                            <i class="fa fa-dashboard"></i> <a href="/loginsys/admin">Dashboard</a>
-                        </li>
-                        <li class="active">
-                            <i class="fa fa-file"></i> Blank Page
-                        </li>
-                    </ol>
-                </div>
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-users fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge"><?= $session->count ?>  </div>
-                                    <div>New Views</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <div>Page View from Gallery</div>
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-green">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-photo fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge"><?= Photo::count() ?> </div>
-                                    <div>Photos</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">Total Photos in Gallery</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
+use Templates\AdminTemplate;
 
 
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-yellow">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-user fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge"><?= User::count() ?>
+$header = '/src/views/partials/admin/header.php';
+$footer =  '/src/views/partials/admin/footer.php';
+$bodyContent = '/src/views/pages/dashboardContent.php';
+$navLink = '/src/views/partials/admin/navigation.php';
 
-                                    </div>
-
-                                    <div>Users</div>
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">Total Users</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-red">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-support fa-5x"></i>
-                                </div>
-                                <!-- <div class="col-xs-9 text-right">
-                                    <div class="huge"><?= Comment::count() ?> </div>
-                                    <div>Comments</div> -->
-                                </div>
-                            </div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">Total Comments</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-
-
-            </div> <!--First Row-->
-            <div id="piechart" style="max-width: 900px; height: 500px;"></div>
-        </div>
-
-    </div>
-<?= include("./includes/footer.php"); ?>
+$template = new AdminTemplate($header, $footer);
+$template->setBody($bodyContent);
+$template->setNav($navLink);
+$template->render();
+?>
