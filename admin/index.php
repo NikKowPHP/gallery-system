5672ab4542e0dc8e51b8822fb3fc0434097d60d0
NikKowPHP <nik.kow@outlook.com>
2024-01-01 08:46:00 +0100

    refactor(IndexAdmin): Update index admin page based on revised admin template

--- admin/index.php
+++ admin/index.php
@@ -1,24 +1,20 @@
 <?php
 use Models\Session;
 use Models\Location;
+use Templates\AdminTemplate;
 
 require_once(__DIR__ . '/../src/views/templates/AdminTemplate.php');
 
 $session = new Session();
 
-if (!$session->is_signed_in()) Location::redirect("src/views/login_page.php");
+if (!$session->is_signed_in())
+	Location::redirect("src/views/login_page.php");
 
 
-use Templates\AdminTemplate;
 
-
-$header = '/src/views/partials/admin/header.php';
-$footer =  '/src/views/partials/admin/footer.php';
 $bodyContent = '/src/views/pages/dashboardContent.php';
-$navLink = '/src/views/partials/admin/navigation.php';
 
-$template = new AdminTemplate($header, $footer);
+$template = new AdminTemplate();
 $template->setBody($bodyContent);
-$template->setNav($navLink);
 $template->render();
 ?>
