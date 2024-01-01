065b7e9996f691ab47619b0c47bd38cf6aee84a6
NikKowPHP <nik.kow@outlook.com>
2024-01-01 08:39:57 +0100

    refactor(AdminTemplate): Update admin template class with autocreation with default header footer and nav links

--- src/views/templates/AdminTemplate.php
+++ src/views/templates/AdminTemplate.php
@@ -1,13 +1,20 @@
 <?php
 namespace Templates;
-require_once(__DIR__ .'/BaseTemplate.php');
+
+require_once(__DIR__ . '/BaseTemplate.php');
 use Templates\BaseTemplate;
 
 
 class AdminTemplate extends BaseTemplate
 {
-	public function __construct($header, $footer)
+	public function __construct()
 	{
-		parent::__construct($header, $footer);
+		$headerLink = '/src/views/partials/admin/header.php';
+		$footerLink = '/src/views/partials/admin/footer.php';
+		$navLink = '/src/views/partials/admin/navigation.php';
+		parent::__construct($headerLink, $footerLink);
+		$this->setNav($navLink);
+
 	}
+
 }
