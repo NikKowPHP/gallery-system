afef0b65a28cd539df0c209cbf2eb9c1230be8b9
NikKowPHP <nik.kow@outlook.com>
2024-01-01 08:43:11 +0100

    refactor(photos): Update photos page with admin template

--- admin/photos.php
+++ admin/photos.php
@@ -1,71 +1,23 @@
-<?php include("includes/header.php"); ?>
-<?php include("includes/top_nav.php"); ?>
-<?php  
-    $photos = Photo::get_all();
+<?php
+use Models\Session;
+use Models\Location;
+use Models\Router;
+
+require_once(__DIR__ . '/../src/views/templates/AdminTemplate.php');
+
+$session = new Session();
+
+if (!$session->is_signed_in())
+  Location::redirect("src/views/login_page.php");
+
+use Templates\AdminTemplate;
+
+$router = new Router();
+$router->add_route(FORMS_PATH . DS . 'admin/photos/add', SITE_ROOT . DS .'/src/views/pages/admin/add_photo.php');
+
+$bodyContent = '/src/views/contents/admin/photos_content.php';
+
+$template = new AdminTemplate();
+$template->setBody($bodyContent);
+$template->render();
 ?>
-    <div id="page-wrapper">
-
-    <div class="container-fluid">
-
-        <!-- Page Heading -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">
-                    Photos
-                    <small>Subheading</small>
-                </h1>
-                <ol class="breadcrumb">
-                    <li>
-                        <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
-                    </li>
-                    <li class="active">
-                        <i class="fa fa-file"></i> Blank Page
-                    </li>
-                </ol>
-            </div>
-        </div>
-        <!-- /.row -->
-        <div class="col-md-12">
-            <table class="table table-hover">
-                <thead>
-                <tr>
-                    <th>Photo</th>
-                    <th>Id</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>File Name</th>
-                    <th>File type</th>
-                    <th>Size</th>
-                    <th>Comments</th>
-                    <th>View</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-									<?php
-									foreach ($photos
-									as $photo):
-                                    $comments_count = Comment::count("photo_id", $photo->id);
-									?>
-                    <td><img width="100" src="<?= $photo->get_file_path() ?>" alt="<?= $photo->title ?>"></td>
-                    <td><?= $photo->id ?></td>
-                    <td><?= $photo->title ?></td>
-                    <td><?= $photo->description ?></td>
-                    <td><?= $photo->filename ?></td>
-                    <td><?= $photo->filetype ?></td>
-                    <td><?= $photo->size ?></td>
-                    <td><a class="btn btn-default" href="/loginsys/admin/comments.php/?id=<?= $photo->id ?>">comments: <?= $comments_count ?> </a></td>
-                    <td><a class="btn btn-info" href="/loginsys/post.php?id=<?= $photo->id ?>">view</a></td>
-                    <td><a class="btn btn-primary" href="edit_photo_page.php/?id=<?= $photo->id ?>">edit</a></td>
-                    <td><a class="btn btn-danger" href="delete_photo.php/?id=<?= $photo->id ?>">delete</a></td>
-                </tr>
-								<?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
-			<?php include_once("includes/sidebar_nav.php"); ?>
-    </div>
-
-<?php include("includes/footer.php"); ?>
