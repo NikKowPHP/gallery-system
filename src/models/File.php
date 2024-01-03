375f2f31c728c0c107a4f36b96f61a7d03157de9
NikKowPHP <nik.kow@outlook.com>
2024-01-03 10:11:06 +0100

    refactor(File): Remove errors array

--- src/models/File.php
+++ src/models/File.php
@@ -13,18 +13,6 @@ class File extends Db_object
 	public ?int $size = null;
 	public ?string $tmp_path = null;
 	public ?string $upload_dir = null;
-
-	public array $errors = array(
-		0 => 'There is no error, the file uploaded with success',
-		1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
-		2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
-		3 => 'The uploaded file was only partially uploaded',
-		4 => 'No file was uploaded',
-		6 => 'Missing a temporary folder',
-		7 => 'Failed to write file to disk.',
-		8 => 'A PHP extension stopped the file upload.',
-
-	);
 	public array $custom_errors = [];
 
 
