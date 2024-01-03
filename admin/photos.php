f5ec88b94187bf3ae650018480ffe82bad6397f0
NikKowPHP <nik.kow@outlook.com>
2024-01-03 10:09:40 +0100

    refactor(constants): Update public folder constant to for right file uploads, remove Router defenitiion from photos.php file

--- admin/photos.php
+++ admin/photos.php
@@ -1,7 +1,6 @@
 <?php
 use Models\Session;
 use Models\Location;
-use Models\Router;
 
 require_once(__DIR__ . '/../src/views/templates/AdminTemplate.php');
 
@@ -12,8 +11,6 @@ if (!$session->is_signed_in())
 
 use Templates\AdminTemplate;
 
-$router = new Router();
-$router->add_route(FORMS_PATH . DS . 'admin/photos/add', SITE_ROOT . DS .'/src/views/pages/admin/add_photo.php');
 
 $bodyContent = '/src/views/contents/admin/photos_content.php';
 
--- config/constants.php
+++ config/constants.php
@@ -3,6 +3,6 @@ defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'code' . DS . 'gallery-system');
 defined('FORMS_PATH') ? null : define('FORMS_PATH', DS . 'gallery-system');
 defined('SRC') ? null : define('SRC', SITE_ROOT . DS . 'src');
-defined('PUBLIC_FOLDER') ? null : define('PUBLIC_FOLDER', FORMS_PATH . DS . 'public');
+defined('PUBLIC_FOLDER') ? null : define('PUBLIC_FOLDER', SITE_ROOT . DS . 'public');
 
 ?>
